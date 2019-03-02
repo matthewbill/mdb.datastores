61e2f7dd2040b1945a565e5432d82141eb3c1cda
matthewbill <matthewdbill@gmail.com>
2019-03-02 23:44:24 +0000

    added sqs wrapper

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 
 const DynamoWrapper = require('./src/dynamo-wrapper.js');
 const S3Wrapper = require('./src/s3-wrapper.js');
+const SqsWrapper = require('./src/sqs-wrapper.js');
 
 module.exports = {
   DynamoWrapper,
   S3Wrapper,
+  SqsWrapper,
 };
---
+++ src/sqs-wrapper.js
@@ -0,0 +1,69 @@
+
+const AWS = require('aws-sdk');
+
+/**
+ * Class representing a wapper for AWS SQS.
+ * @class SqsWrapper
+ */
+class SqsWrapper {
+  /**
+   * Constructor for SqsWrapper
+   * @param {string} options.region The AWS region.
+   * @param {Logger} options.logger The logger.
+   */
+  constructor(options) {
+    const self = this;
+    self.logger = options.logger;
+    self.sqs = new AWS.SQS({ region: options.region });
+  }
+
+  /**
+   * Sends a message to SQS
+   * @param {string} options.messageBody The message to send.
+   * @param {string} options.queueUrl The SQS queue url.
+   * @returns {Promise} Promise to send a mesage to SQS.
+   * @memberof SqsWrapper
+   */
+  sendMessage(options) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      var params = {
+        MessageBody: options.messageBody,
+        QueueUrl: options.queueUrl,
+        DelaySeconds: 0,
+      };
+      self.sqs.sendMessage(params, function(err, data) {
+        if (err) {
+          self.logger.error(err, err.stack);
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  /**
+   * Receives a message from SQS.
+   * @param {string} options.queueUrl The SQS queue url.
+   * @returns {Promise} Promise to receieve a mesage from SQS.
+   * @memberof SqsWrapper
+   */
+  receiveMessage(options) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      const params = {
+        QueueUrl: options.queueUrl,
+        WaitTimeSeconds: 20
+      };
+      self.sqs.receiveMessage(params, function(err, data) {
+        if (err) {
+          self.logger.error(err, err.stack);
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
+}
+
+module.exports = SqsWrapper;
